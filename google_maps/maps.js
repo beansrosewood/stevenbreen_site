dbdad5a0bbc22320b99ea5f99aaef4c90b379f42
beansrosewood <57677017+beansrosewood@users.noreply.github.com>
2020-12-05 21:28:18 -0500

    Update maps.js

--- google_maps/maps.js
+++ google_maps/maps.js
@@ -7,46 +7,60 @@ var harvard_yard_map;
 function initMap() {
     console.log("I'm in initmap");
     var statue_latlng = new google.maps.LatLng(42.374474, -71.117207);
-var northwest_latlng = new google.maps.LatLng(42.3795462, -71.1156024);
+    var northwest_latlng = new google.maps.LatLng(42.3795462, -71.1156024);
+    var chez_sheridan_latlng = new google.maps.LatLng(42.776370, -71.478260);
     var mapOptions = {
         zoom: 15,
         center: new google.maps.LatLng(42.377, -71.1164)
     };
     harvard_yard_map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
-    
+        mapOptions);
+
     var statue_info = "<strong>John Harvard Statue</strong><br/>" +
         "<a href='https://www.summer.harvard.edu/inside-summer/3-lies-harvard'>3 Lies of Harvard</a>";
-        
-    var northwest_info = "<strong>Northwest Science Building</strong><br/>" + "Latitude: 42.3795462<br/>" + "Longitude: -71.1156024";    
+
+    var northwest_info = "<strong>Northwest Science Building</strong><br/>" + "Latitude: 42.3795462<br/>" + "Longitude: -71.1156024";
     
+    var chez_sheridan_info = "<strong>Chez Sheridan Office/Shop</strong><br/>" + "9 Sheridan St" + "Nashua, NH 03064";
+
     var statue_infowindow = new google.maps.InfoWindow({
         content: statue_info
     });
-    
+
     var northwest_infowindow = new google.maps.InfoWindow({
-    		content: northwest_info
-        
+        content: northwest_info
     });
     
+     var sheridan_infowindow = new google.maps.InfoWindow({
+        content: chez_sheridan_info
+    });
+
     var statue_marker = new google.maps.Marker({
-      position: statue_latlng,
-      map: harvard_yard_map,
-      title: 'John Harvard Statue'
+        position: statue_latlng,
+        map: harvard_yard_map,
+        title: 'John Harvard Statue'
     });
-    
+
     var northwest_marker = new google.maps.Marker({
-    	position: northwest_latlng,
-      map: harvard_yard_map,
-      title: 'Northwest Science Building'
+        position: northwest_latlng,
+        map: harvard_yard_map,
+        title: 'Northwest Science Building'
     });
     
-    google.maps.event.addListener(statue_marker, 'click', function() {
-      statue_infowindow.open(harvard_yard_map,statue_marker);
+    var chez_sheridan_marker = new google.maps.Marker({
+        position: chez_sheridan_latlng,
+        map: harvard_yard_map,
+        title: 'Chez Sheridan Office/Shop'
     });
-    google.maps.event.addListener(northwest_marker, 'click', function() {
-    	northwest_infowindow.open(harvard_yard_map,northwest_marker);
+
+    google.maps.event.addListener(statue_marker, 'click', function () {
+        statue_infowindow.open(harvard_yard_map, statue_marker);
+    });
+    google.maps.event.addListener(northwest_marker, 'click', function () {
+        northwest_infowindow.open(harvard_yard_map, northwest_marker);
+    });
+    
+    google.maps.event.addListener(chez_sheridan_marker, 'click', function () {
+        sheridan_infowindow.open(harvard_yard_map, chez_sheridan_marker);
     });
 }
-
-
