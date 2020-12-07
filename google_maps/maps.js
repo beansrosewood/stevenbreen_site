510858be17d0cbaa3920a871aa868661c73d2bb4
beansrosewood <57677017+beansrosewood@users.noreply.github.com>
2020-12-07 18:50:16 -0500

    Update maps.js

--- google_maps/maps.js
+++ google_maps/maps.js
@@ -9,8 +9,9 @@ function initMap() {
     var statue_latlng = new google.maps.LatLng(42.374474, -71.117207);
     var northwest_latlng = new google.maps.LatLng(42.3795462, -71.1156024);
     var chez_sheridan_latlng = new google.maps.LatLng(42.776370, -71.478260);
+    var precision_coating_latlng = new google.maps.LatLng(42.385200, -71.494520);
     var mapOptions = {
-        zoom: 15,
+        zoom: 8,
         center: new google.maps.LatLng(42.377, -71.1164)
     };
     harvard_yard_map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -23,6 +24,8 @@ function initMap() {
     
     var chez_sheridan_info = "<strong>Chez Sheridan Office/Shop</strong><br/>" + "9 Sheridan St" + "Nashua, NH 03064";
 
+    var precision_coating_info = "<strong>Medical Device Coater</strong><br/>";
+    
     var statue_infowindow = new google.maps.InfoWindow({
         content: statue_info
     });
@@ -35,6 +38,10 @@ function initMap() {
         content: chez_sheridan_info
     });
 
+    var precision_coating_infowindow = new google.maps.InfoWindow({
+        content: precision_coating_info
+    });
+    
     var statue_marker = new google.maps.Marker({
         position: statue_latlng,
         map: harvard_yard_map,
@@ -53,6 +60,12 @@ function initMap() {
         title: 'Chez Sheridan Office/Shop'
     });
 
+    var precision_coating_marker = new google.maps.Marker({
+        position: precision_coating_latlng,
+        map: harvard_yard_map,
+        title: 'Precision Coating'
+    });
+    
     google.maps.event.addListener(statue_marker, 'click', function () {
         statue_infowindow.open(harvard_yard_map, statue_marker);
     });
@@ -62,5 +75,8 @@ function initMap() {
     
     google.maps.event.addListener(chez_sheridan_marker, 'click', function () {
         sheridan_infowindow.open(harvard_yard_map, chez_sheridan_marker);
+    });
+    google.maps.event.addListener(precision_coating_marker, 'click', function () {
+        precision_coating_infowindow.open(harvard_yard_map, precision_coating_marker);
     });
 }
