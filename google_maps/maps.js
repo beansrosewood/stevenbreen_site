fe3ab0b70a5256941cb80222a3fb472c95619ab4
beansrosewood <57677017+beansrosewood@users.noreply.github.com>
2021-01-06 08:45:43 -0500

    Update maps.js

--- google_maps/maps.js
+++ google_maps/maps.js
@@ -92,7 +92,7 @@ function initMap() {
     
     var mapOptions = {
         zoom: 7,
-        center: new google.maps.LatLng(42.50, -75.75),
+        center: new google.maps.LatLng(42.75, -75.75),
         mapTypeId: 'hybrid'
     };
     
