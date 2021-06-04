b1e1915b483670e1d8b1ca403e2eea32ff4e71b6
beansrosewood <57677017+beansrosewood@users.noreply.github.com>
2021-06-04 14:12:40 -0400

    css

--- cheeseburger.js
+++ cheeseburger.js
@@ -1,13 +1,13 @@
 
         /*CHEESEBURGER ICON SCRIPT*/
+function myFunction()
+$(".hamburger")
+    .append("<div/><div/><div/>")
+    .click(function (e) {
+        $(this).toggleClass("expanded");
+        e.stopPropogation();
+    });
 
-        $(".hamburger")
-            .append("<div/><div/><div/>")
-            .click(function(e) {
-                $(this).toggleClass("expanded");
-                e.stopPropogation();
-            });
-
-        $("body").click(function(e) {
-            $(".expanded").removeClass("expanded");
-        });
+$("body").click(function (e) {
+    $(".expanded").removeClass("expanded");
+});
