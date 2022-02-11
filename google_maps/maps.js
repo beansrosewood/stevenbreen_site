f06a8b9f7ed19ee13920411bd49a063658c24a71
beansrosewood <57677017+beansrosewood@users.noreply.github.com>
2022-02-10 23:33:39 -0500

    Update maps.js

--- google_maps/maps.js
+++ google_maps/maps.js
@@ -2,11 +2,11 @@ var harvard_yard_map;
 /* John Harvard Status
 42.374474,-71.117207 */
 
-
-let overview;
-const OVERVIEW_DIFFERENCE = 5;
-const OVERVIEW_MIN_ZOOM = 3;
-const OVERVIEW_MAX_ZOOM = 10;
+//Below is for possible overview map.
+//let overview;
+//const OVERVIEW_DIFFERENCE = 5;
+//const OVERVIEW_MIN_ZOOM = 3;
+//const OVERVIEW_MAX_ZOOM = 10;
 
 
 
@@ -190,27 +190,27 @@ function initMap() {
     
     
     // instantiate the overview map without controls
-  overview = new google.maps.Map(document.getElementById("overview"), {
-    ...mapOptions,
-    disableDefaultUI: true,
-    gestureHandling: "none",
-    zoomControl: false,
-  });
+  //overview = new google.maps.Map(document.getElementById("overview"), {
+   // ...mapOptions,
+   // disableDefaultUI: true,
+    //gestureHandling: "none",
+   // zoomControl: false,
+  //});
 
-  function clamp(num, min, max) {
-    return Math.min(Math.max(num, min), max);
-  }
+  //function clamp(num, min, max) {
+   // return Math.min(Math.max(num, min), max);
+  //}
 
-  map.addListener("bounds_changed", () => {
-    overview.setCenter(map.getCenter());
-    overview.setZoom(
-      clamp(
-        map.getZoom() - OVERVIEW_DIFFERENCE,
-        OVERVIEW_MIN_ZOOM,
-        OVERVIEW_MAX_ZOOM
-      )
-    );
-  });
+  //map.addListener("bounds_changed", () => {
+   // overview.setCenter(map.getCenter());
+   // overview.setZoom(
+     // clamp(
+       // map.getZoom() - OVERVIEW_DIFFERENCE,
+       // OVERVIEW_MIN_ZOOM,
+        //OVERVIEW_MAX_ZOOM
+     // )
+   // );
+  //});
 
    //end of overview map. Also, includes the three const. near the top. 
     
